794e18cebd55c1dd0eaa9ae22906999a540f14c3
Hoa <hoameomu@gmail.com>
2021-03-21 13:25:38 +0800

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+
+export default {
+    input: 'index.ts',
+    output: [
+        {
+            format: 'umd',
+            file: 'index.js',
+            name: 'colorbewer'
+        }
+    ],
+    plugins: [
+        typescript({
+            declaration: true,
+            rootDir: './',
+            outDir: './build'
+        }),
+        commonjs(),
+        resolve()
+    ]
+}
